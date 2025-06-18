f4820b71b6d2daa29793113ed89ee737a4ccc5ac
Marmik23 <007marmiknpatel@gmail.com>
2025-06-18 10:07:36 -0400

    First capstone commit

---
+++ InvestRed/src/shared/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../index.css';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo">InvestRed</div>
+      <div className="nav-links">
+        <span>Investments</span>
+        <Link to="/loan">Loans</Link> 
+        <Link to="/insurance">Insurance</Link>
+        {/* <span>Mortgage</span> */}
+        <span>Blogs</span>
+        <span>Account</span>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
